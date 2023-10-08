e4c0f2a176568535f7821bd24439ad94f182b399
Hayden Connor <hconnor3@gmail.com>
2023-10-07 20:07:27 -0600

    Basic version for testing

---
+++ build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    slint_build::compile_with_config(
+        "ui/appwindow.slint",
+        slint_build::CompilerConfiguration::new()
+            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+    )
+    .unwrap();
+}
+
+// fn main() {
+//     let config = slint_build::CompilerConfiguration::new()
+//         .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
+//     slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
+//     slint_build::print_rustc_flags().unwrap();
+// }
